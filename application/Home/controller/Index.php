20edcf00b6efd4872814402c51848954e083703f
Eagle <617701021@qq.com>
2017-09-07 21:48:39 +0800

    fetch 使用以及数据库的操作方法

--- application/Home/controller/Index.php
+++ application/Home/controller/Index.php
@@ -1,31 +1,21 @@
 <?php
 namespace app\Home\controller;
-use think\console\command\make\Controller;
 use think\Db;
 use think\View;
-use think\Model;
+use think\Controller;
 
-class Index extends \Think\Controller
+class Index extends Controller
 {
 
     public function index(){
         $return = M('admin')->select();  // 操作数据库获取数据第二种方法 Tp5不支持M()要自己3.2需要自已定义
         //测试TP5用法
         echo M('admin')->getLastSql();  //只有这一种获取最后一条Sql方法   _sql不支持
-
         $sql = "select * from tp5_admin";
         $return = Db::query($sql);  // 操作数据库获取数据第二种方法
         return $this->fetch();
-        // fetch暂不能使用，待解决
+        // fetch方法使用已解决   解决方法：加载Controller类不对 应为think\Controller
 //        return 'Home->Index->index';
-
-        // 推送到test1分支上
-
-        // 第二次推送到test1分支上的修改
-
-        // 第三次推送请求
-
-
 
     }
 
