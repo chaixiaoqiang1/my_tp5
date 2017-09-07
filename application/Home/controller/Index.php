3adafac8365ef3be84d7e3b0d14e5d2cf684d781
Eagle <617701021@qq.com>
2017-09-07 22:04:29 +0800

    tp5与tp3的模板赋值方式是一样的

--- application/Home/controller/Index.php
+++ application/Home/controller/Index.php
@@ -13,6 +13,7 @@ class Index extends Controller
         echo M('admin')->getLastSql();  //只有这一种获取最后一条Sql方法   _sql不支持
         $sql = "select * from tp5_admin";
         $return = Db::query($sql);  // 操作数据库获取数据第二种方法
+        $this->assign('data',$return);
         return $this->fetch();
         // fetch方法使用已解决   解决方法：加载Controller类不对 应为think\Controller
 //        return 'Home->Index->index';
