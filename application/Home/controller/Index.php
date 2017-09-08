376f6af42e97711194ef9795ba5fd2ce265c59de
Eagle <617701021@qq.com>
2017-09-09 07:16:35 +0800

    tp5与tp3的模板赋值方式是一样的

--- application/Home/controller/Index.php
+++ application/Home/controller/Index.php
@@ -8,6 +8,10 @@ class Index extends Controller
 {
 
     public function index(){
+        /*if($id){
+            var_dump($id);die;
+        }*/
+//        $gid = $_GET['id'];
         $return = M('admin')->select();  // 操作数据库获取数据第二种方法 Tp5不支持M()要自己3.2需要自已定义
         // 测试TP5用法
         // echo M('admin')->getLastSql();  //只有这一种获取最后一条Sql方法   _sql不支持
