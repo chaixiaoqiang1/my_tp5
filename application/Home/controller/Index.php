0238f6a4da2a0993f33cfd29ae0f70b6f53a5adc
Eagle <617701021@qq.com>
2017-09-12 10:46:36 +0800

    测试代码
    验证登录验证码

--- application/Home/controller/Index.php
+++ application/Home/controller/Index.php
@@ -110,6 +110,40 @@ class Index extends Controller
         $captcha->zhSet = '们以我到他会作时要动国产的一是工就年阶义发成部民可出能方进在了不和有大这';
         return  $captcha->entry();
     }
+    
+    //批量导入
+    public function orderImport(Request $request)
+    {
+        if ($request->isPost()) {
+            if ($request->post('name') == 1) {
+            }
+            $res = $this->validate(input('post.'), 'Order');
+            if (!captcha_check(input('post.captcha'))) {
+                $this->result([], 0, 'error', 'json');
+            };
+            if ($res !== true) {
+                $this->result([], 0, $res, 'json');
+            }
+            dump($res);
+            exit;
+            $file = $request->file('order');
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->validate(['size' => 100000, 'type' => array()])->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'orders');
+            if ($info) {
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info->getExtension();
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getSaveName();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getFilename();
+            } else {
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
+        return $this->fetch();
+    }
 
 
 
