afe5832cdc8a1365bef9bceabef8cee9ca436f56
Eagle <617701021@qq.com>
2017-09-21 22:17:16 +0800

    Revert "模板继承等使用"
    This reverts commit b1d5d228989727981500d36dd3c04f2ffda43458.
    
    # Conflicts:
    #	application/Home/controller/Index.php
    #	application/Home/view/public/base.html

--- index.php
+++ index.php
@@ -6,7 +6,5 @@
 // 定义应用目录
 
 define('APP_PATH', __DIR__ . '/application/');
-
-define("PUBLIC_PATH",'/test/my_tp5/public/home'); //定义public目录  懒人一个 不想在配置文件  进行修改
 // 加载框架引导文件
 require __DIR__ . '/thinkphp/start.php';
